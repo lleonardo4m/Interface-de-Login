b22d66bad358e15b6f19a84b5cc8661ed5bd4987
leonardo barbosa <lleonardo.4m@gmail.com>
2025-01-22 01:29:54 -0300

    img

--- login.php
+++ login.php
@@ -1,3 +1,39 @@
+<?php
+include_once('config.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu email";
+    }   else if(strlen($_POST['senha']) == 0){
+        echo "Preencha sua senha";
+    }  else {
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM formulario_resenhando.usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução co codigo sql" . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+
+            $usuario = $sql_query->fetc_assoc();
+
+            if(!isset($_SESSION)){
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: paginatest.php");
+        } 
+        else {
+            echo "Error";
+        }
+        }
+}  # code...
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
--- sistema.php
+++ sistema.php
@@ -7,122 +7,330 @@ if((!isset($_SESSION['nome']) == true) and (!isset($_SESSION['senha']) == true))
             unset($_SESSION['senha']);                
     header('location: login.php');
 }
+
 $logado = $_SESSION['nome'];
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resenhando</title>
-    
     <style>
-        *{
-            margin: 0;
-            padding: 0;
-        }
+    *{
+        margin: 0;
+        padding: 0;
 
-        a{
-            color: black;
-            text-decoration: none;
-            transition: 0.3s;
-        }
+        box-sizing: border-box;
+    }
 
-        a:hover{
-            opacity: 0.7;
-        }
+    header{
+        width: 100%;
+        height: 50px;
 
-        .logo{
-            font-size: 24px;
-            text-transform: uppercase;
-            font-family: cursive;
-        }
+        background: linear-gradient(to right,#32ff5f, rgb(0, 238, 255));
+        
+        display: flex;
+        justify-content: center;
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.4);
+    }
 
-        nav{
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            height: 8vh;
-            border: solid, 1px,rgb(50, 255, 95);
-        }
-        .nav-list{
-            list-style: none;
-            display: flex;
-        }
-        .nav-list li{
-            letter-spacing: 3px;
-            margin-left: 32px;
-        }
-        .mobile-menu div{
-            width: 32px;
-            height: 2px;
-            background: black;
-            margin: 8px;
-            cursor: pointer;
-        }
-        @media (max-width: 999px){
-            body{
-                overflow-x: hidden;
-            }
-            .nav-list{
-                position: absolute;
-                top: 8vh;
-                right: 0;
-                width: 50vw;
-                height: 92vh;
-                flex-direction: column;
-                align-items: center;
-                justify-content: space-around;
-                transform: translateX(100%);
-        }
+    body{       
+        background: #fbfbfb;
+        font-family: "roboto";
 
-        .nav-list li {
-            margin-left: 0;
-            opacity: 0;
-        }
+        -webkit-font-smoothing: antialiased !important;
+    }
 
-        .mobile-menu {
-            display: block;
-        }
+    li{
+        list-style: none;
+    }
 
-        .nav-list.active {
-            transform: translateX(0);
-        }
+    .btnImg {
+        height: 20px;
+    }
 
-        }
+    .main{
+        max-width: 900px;
+        margin: 0 auto;
+    }
+
+    div.newPost{
+        width: 100%;
+        height: 240px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: 50px 0;
+        padding: 25px;
+
+        background: #fff;
+        border-radius: 10px;
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.2);
+    }
+
+    div.inforUser {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    div.imgUser {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        background: #333;
+    }
+
+    .nameAndHour_me{
+        font-size: 16px;
+
+        color:#727272;
+        margin-left: 10px;
+    }
+
+    h1{
+        font-family: cursive;
+    }
+
+    .formPost{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+    .formPost textarea{
+        height: 90px;
+        background: #fbfbfb;
+        margin: 15px 0;
+        padding: 25px;
+        border-radius: 10px;
+        resize: none;
+        border: 0;
+        outline: none;
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.4);
+        
+        font-weight: bold;
+        font-family: 'roboto';
+        color: #727272;
+    }
+
+    .iconsAndButton{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .icons{
+        width: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    div.icons button{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        outline: none;
+        border: none;
+        margin-right: 15px;
+
+        background: #fff;
+        cursor: pointer;
+    }
+
+    .btnSubmitForm{
+        background: #32ff5f;
+        padding: 10px 50px;
+        color: #fff;
+        font-weight: bold;
+        outline: none;
+        cursor: pointer;
+        border: none;
+        border-radius: 10px;
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.4);
+    }
+
+    .btnSubmitForm:hover{
+        background-color:rgb(30, 229, 73);
+        transform: scale(1.05);
+        transition: .6s;
+    }
+
+    li.post{
+        margin-bottom: 50px;
+        background: #fff;
+        border-radius: 10px;
+        padding: 25px;
+        width: 100%;
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.1);
+    }
+
+    .inforUserPost{
+        display: flex;
+        align-items: center;
+    }
+
+    .nameAndHour{
+        margin-left: 10px;
+    }
+
+    .imgUserPost{
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        background: #727272;
+    }
+
+    .nameAndHour_user{
+        color: #727272;
+    }
+
+    .nameAndHour p{
+        color: #D1D1D1;
+        font-size: 12px;
+    }
+
+    .comentario{
+        width: 100%;
+        margin: 20px 0;
+        font-weight: bold;
+        color: #727272;
+    }
+
+    .filesPost{
+        box-shadow: 1px 0 3px rgba(0, 0, 0, 0.4);
+        border: none;
+        outline: none;
+        cursor: pointer;
+        margin-right: 10px;
+        padding: 5px 20px;
+        border-radius: 10px;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background 0.2s;
+        color: #fff;
+    }
+
+    .like{
+        background-color:#fd4747;
+    }
+
+    .like:hover{
+        background-color:#9c0101;
+    }
+
+    .coment{
+        background: #ffc320 ;
+    }
+
+    .coment:hover{
+        background:#cc9500 ;
+    }
+
+    .share{
+        background: #23A0ff;
+    }
+
+    .share:hover{
+        background:#0091ff;
+    }
     </style>
 </head>
 <body>
 
-<header>
-    <nav>
-        <a class="logo" href="/">Resenhando</a>
-        
-        <div class="mobile-menu">
+    <header>
+        <h1>Resenhando</h1>
+    </header>
+    
+    <main class="main">
 
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
+        <div class="newPost">
+            <div class="inforUser">
+                <div class="imgUser"></div>
+                <strong class="nameAndHour_me">Leonardo</strong>
+            </div>
 
+            <form action="" class="formPost">
+                
+                <textarea name="textarea" placeholder="O que você esta pensando?"></textarea>
+                <div class="iconsAndButton">
+                    <div class="icons">
+                        <button class="btnFileForm"><img src="img/img.png" alt="Adicionar uma imagem" class="btnImg"></button>
+                        <button class="btnFileForm"><img src="img/cam.png" alt="Adicionar um video" class="btnImg"></button>
+                    </div>
+
+                    <button type="submit" class="btnSubmitForm">Publicar</button>
+                </div>
+            </form>
         </div>
-        
-        <ul class="nav-list">
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/">Sobre</a></li>
-            <li><a href="/">Projetos</a></li>
-            <li><a href="/">Contato</a></li>
 
+        <ul class="posts">
+            <li class="post">
+                <div class="inforUserPost">
+                   <div class="imgUserPost"></div> 
+
+                   <div class="nameAndHour">
+                    <strong class="nameAndHour_user">Usuario</strong>
+                    <p>21h</p>
+                   </div>
+                </div>
+
+                <p class="comentario">
+                    TEXTO ALEATORIO
+                </p>
+
+                <div class="actionBtnPost">
+                    <button type="button" class="filesPost like">curtir</button>
+                    <button type="button" class="filesPost coment">comentar</button>
+                    <button type="button" class="filesPost share">compartilhar</button>
+                </div>
+            </li>
+
+            <li class="post">
+                <div class="inforUserPost">
+                   <div class="imgUserPost"></div> 
+
+                   <div class="nameAndHour">
+                    <strong class="nameAndHour_user">Usuario</strong>
+                    <p>21h</p>
+                   </div>
+                </div>
+
+                <p class="comentario">
+                    TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO
+                </p>
+
+                <div class="actionBtnPost">
+                    <button type="button" class="filesPost like">curtir</button>
+                    <button type="button" class="filesPost coment">comentar</button>
+                    <button type="button" class="filesPost share">compartilhar</button>
+                </div>
+            </li>
+
+            <li class="post">
+                <div class="inforUserPost">
+                   <div class="imgUserPost"></div> 
+
+                   <div class="nameAndHour">
+                    <strong class="nameAndHour_user">Usuario</strong>
+                    <p>21h</p>
+                   </div>
+                </div>
+
+                <p class="comentario">
+                    TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO
+                    TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO TEXTO ALEATORIO
+                </p>
+
+                <div class="actionBtnPost">
+                    <button type="button" class="filesPost like">curtir</button>
+                    <button type="button" class="filesPost coment">comentar</button>
+                    <button type="button" class="filesPost share">compartilhar</button>
+                </div>
+            </li>
         </ul>
-
-    </nav>
-
-</header>
-
-<script src="mobile-navbar.js"></script>
-
+    </main>
 </body>
-
 </html>
